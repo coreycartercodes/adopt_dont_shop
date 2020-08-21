46230545b822ddd686f2557823fe7ba21b17095d
Corey Carter <crc2231@gmail.com>
2020-08-20 20:57:06 -0600

    Add New Shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -3,8 +3,23 @@ class SheltersController < ApplicationController
     @shelters = Shelter.all
   end
 
+  def new
+  end
+  
   def show
     @shelter = Shelter.find(params[:id])
   end
+
+  #Might consider doing shelter_params method
+  def create
+    Shelter.create(shelter_params)
+    redirect_to '/shelters'
+  end
+
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
+  end
+
+
 
 end
